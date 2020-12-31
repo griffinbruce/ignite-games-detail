422a4f3b8f39ffdf5f19470bfd2b4203bf6127d1
Griffin Schulert <griffin.bruce@gmail.com>
2020-12-31 17:06:37 -0500

    Global Styles and Expanding Games List

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 //Components and Pages
 import Home from './pages/Home';
+//Stylying
+import GlobalStyles from './components/GlobalStyles';
 
 function App() {
   return (
     <div className='App'>
+      <GlobalStyles />
       <Home />
     </div>
   );
