1ebc0d61c2e3cd1fd203d077a85da6fba111889f
Griffin Schulert <griffin.bruce@gmail.com>
2021-08-04 08:48:12 -0400

    update API key and add env

--- src/api.js
+++ src/api.js
@@ -1,32 +1,41 @@
+import axios from "axios";
+import moment from "moment";
+
+require("dotenv").config();
+
 //Base URL
-const base_url = 'https://api.rawg.io/api/';
+//const base_url = 'https://api.rawg.io/api/';
+const rawgAxios = axios.create({
+  base_url: "https://api.rawg.io/api",
+
+  params: {
+    key: process.env.REACT_APP_RAWG_API,
+  },
+});
 
 //Getting the date
-const getCurrentMonth = () => {
-  const month = new Date().getMonth() + 1;
-  if (month < 10) {
-    return `0${month}`;
-  } else {
-    return month;
-  }
-};
+// const getCurrentMonth = () => {
+//   const month = new Date().getMonth() + 1;
+//   if (month < 10) {
+//     return `0${month}`;
+//   } else {
+//     return month;
+//   }
+// };
 
-const getCurrentDay = () => {
-  const day = new Date().getDate();
-  if (day < 10) {
-    return `0${day}`;
-  } else {
-    return day;
-  }
-};
+// const getCurrentDay = () => {
+//   const day = new Date().getDate();
+//   if (day < 10) {
+//     return `0${day}`;
+//   } else {
+//     return day;
+//   }
+// };
 
 //Current Day/Month/Year
-const currentYear = new Date().getFullYear();
-const currentMonth = getCurrentMonth();
-const currentDay = getCurrentDay();
-const currentDate = `${currentYear}-${currentMonth}-${currentDay}`;
-const lastYear = `${currentYear - 1}-${currentMonth}-${currentDay}`;
-const nextYear = `${currentYear + 1}-${currentMonth}-${currentDay}`;
+const currentDate = `${moment().format('YYYY-MM-DD')}`;
+const lastYear = `${moment().subtract(1, 'year').format('YYYY-MM-DD')}`;
+const nextYear = `${moment().add(1, 'year').format('YYYY-MM-DD')}`;
 
 //Popular Games
 const popular_games = `games?dates=${lastYear},${currentDate}&ordering=-rating&page_size=10`;
